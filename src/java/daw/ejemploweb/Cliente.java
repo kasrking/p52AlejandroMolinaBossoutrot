d0dbaeaa389defb6b81c90b5e1923722d948d51a
kasrking <kasrking.juegos@gmail.com>
2019-03-04 18:16:45 +0100

    Generar descriptor de despliegue básico y otros cambios anteriores

--- src/java/daw/ejemploweb/Cliente.java
+++ src/java/daw/ejemploweb/Cliente.java
@@ -13,7 +13,51 @@ public class Cliente {
     
     private String nombre;
     private int edad;
-    private String mail;
+    private String mail;  
+
+    // CONSTRUCTOR
+    public Cliente(String nombre, int edad, String mail) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.mail = mail;
+    }
+
+    // GETTERS    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    // SETTERS    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    // toString
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", edad=" + edad + ", mail=" + mail + '}';
+    }
+    
+    
+    
+    
     
     
     
